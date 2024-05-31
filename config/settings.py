6654dc16db1497533ab7d1eaeb2c3e2783c66395
Amir Shabani <amirshabaniv@gmail.com>
2024-06-01 03:22:03 +0330

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'jobinja',
         'USER': 'postgres',
-        'PASSWORD': 'amir4014',
+        'PASSWORD': 'pass',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -151,7 +151,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'amirshabaniv@gmail.com'
-EMAIL_HOST_PASSWORD = 'ysovtddfvhenhuxs'
+EMAIL_HOST_PASSWORD = 'pass'
 EMAIL_USE_TLS = True
 
 
